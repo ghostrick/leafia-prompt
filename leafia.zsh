c374dccb3f9fe7712e06f095d391ccba123ae85b
_Ghostrick_ <ghostrick.kinako@gmail.com>
2017-11-17 01:00:08 +0900

    change dir style

--- leafia.zsh
+++ leafia.zsh
@@ -1,13 +1,17 @@
 function prompt_leafia_setup {
-  # Define prompts.
-  PROMPT='%c%f | %f '
+
+    add-zsh-hook precmd prompt_leafia_precmd
+    PROMPT='>'
 }
 
 function prompt_leafia_precmd {
+
+    Color=(35 114 228)
+    print -P "\e[38;5;$Color[2]m%d%f"
 }
 
 function prompt_leafia_preview {
-  prompt_preview_theme 'leafia'
+    prompt_preview_theme 'leafia'
 }
 
 prompt_leafia_setup '$@'
