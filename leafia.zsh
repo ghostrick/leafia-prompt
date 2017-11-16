ca4b2dd953774985dc946cf5d10cb418a619e61f
_Ghostrick_ <ghostrick.kinako@gmail.com>
2017-11-17 04:02:03 +0900

    add syntax highlight

--- leafia.zsh
+++ leafia.zsh
@@ -1,12 +1,27 @@
+
+# add color palette
+Color=(35 114 230 227)
+
+# setting syntax-highlighting color
+typeset -g -A ZSH_HIGHLIGHT_STYLES
+ZSH_HIGHLIGHT_STYLES[command]="fg=$Color[2]"
+ZSH_HIGHLIGHT_STYLES[alias]="fg=$Color[2]"
+ZSH_HIGHLIGHT_STYLES[path]="fg=$Color[1]"
+ZSH_HIGHLIGHT_STYLES[globbing]="fg=$Color[3]"
+ZSH_HIGHLIGHT_STYLES[unknown-token]="fg=245"
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]="fg=$Color[4]"
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]="fg=$Color[4]"
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]="fg=$Color[4]"
+
 function prompt_leafia_setup {
+
+    zstyle ':prezto:module:syntax-highlighting' color 'yes'
 
     add-zsh-hook precmd prompt_leafia_precmd
     PROMPT='🍃 '
 }
 
 function prompt_leafia_precmd {
-
-    Color=(35 114 230)
 
     if (( $+functions[git-info] )); then
         git-info
